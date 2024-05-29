101d104174f65031f876f9a877869f13ef1203eb
agustuscaeza <muhammadizzat250801@gmail.com>
2024-05-29 16:39:25 +0800

    update health_process.php

---
+++ healthy-habits-backend/health_process.php
@@ -0,0 +1,41 @@
+<?php
+include('../healthy-habits-backend/database/db_connection.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $height = $_POST["height"];
+    $weight = $_POST["weight"];
+
+    // Calculate BMI
+    $bmi = $weight / ($height * $height);
+
+    // Determine BMI status
+    if ($bmi < 18.5) {
+        $bmiStatus = 'Underweight';
+    } elseif ($bmi >= 18.5 && $bmi < 25) {
+        $bmiStatus = 'Normal weight';
+    } elseif ($bmi >= 25 && $bmi < 30) {
+        $bmiStatus = 'Overweight';
+    } else {
+        $bmiStatus = 'Obese';
+    }
+
+    // Insert health data into database
+    $sqlInsert = "INSERT INTO Health_Data (height, weight, bmi, bmi_status) 
+                  VALUES ('$height', '$weight', '$bmi', '$bmiStatus')";
+
+    $result = mysqli_query($con, $sqlInsert);
+
+    if ($result) {
+        // Display BMI and BMI status to the user
+        echo "<script>
+                window.alert('Health data inserted successfully.');
+                window.location.href = '../html/homepage2.html';
+              </script>";
+    } else {
+        echo "<script>
+                window.alert('Failed to insert health data.');
+                window.location.href = 'PersonalizedHealthAssessment.html';
+              </script>";
+    }
+}
+?>
