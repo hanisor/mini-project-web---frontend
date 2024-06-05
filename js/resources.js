60266adc13c62d99fb364b31ea6d499367f1c97f
agustuscaeza <muhammadizzat250801@gmail.com>
2024-06-05 16:59:50 +0800

    Update resources.js

--- js/resources.js
+++ js/resources.js
@@ -1,13 +1,14 @@
 const resources = {
     healthyLifestyle: [
-        { type: 'article', title: '10 Tips for a Healthy Lifestyle', link: 'https://example.com/lifestyle1' },
-        { type: 'video', title: 'Healthy Living: The Basics', link: 'https://www.youtube.com/watch?v=xyQY8a-ng6g&list=PLq_kAHHySqw5p716lV_JvLmydZkbO3RJe&pp=gAQBiAQB' },
+        { type: 'article', title: '10 Tips for a Healthy Lifestyle', link: 'https://www.medicinenet.com/what_are_10_tips_for_a_healthy_lifestyle/article.htm' },
+        { type: 'video', title: 'Healthy Living Tips', link: 'https://www.youtube.com/watch?v=c06dTj0v0sM&list=PLq_kAHHySqw7z2LoED6WfVFda5TmfePzg&pp=gAQBiAQB' },
     ],
     healthyFood: [
-        { type: 'article', title: 'Top 10 Superfoods for a Healthy Diet', link: 'https://example.com/food1' },
-        { type: 'video', title: 'Cooking Healthy Meals at Home', link: 'https://www.youtube.com/watch?v=c06dTj0v0sM&list=PLq_kAHHySqw7z2LoED6WfVFda5TmfePzg&pp=gAQBiAQB' },
+        { type: 'article', title: 'Top 10 foods for a Healthy Diet', link: 'https://www.ucsfhealth.org/education/top-ten-foods-for-health' },
+        { type: 'video', title: 'Benefits Healthy Food', link: 'https://www.youtube.com/watch?v=xyQY8a-ng6g&list=PLq_kAHHySqw5p716lV_JvLmydZkbO3RJe&pp=gAQBiAQB' },
     ]
 };
+
 
 function filterResources(category) {
     const resourceContainer = document.getElementById('resources');
